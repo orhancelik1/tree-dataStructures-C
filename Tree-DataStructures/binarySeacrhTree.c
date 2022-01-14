a94a8384766a78562abc2eae573a92f67c4c18c9
orhan <orhankagan33@gmail.com>
2022-01-14 14:48:25 +0300

    implementing BST

---
+++ Tree-DataStructures/binarySeacrhTree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Bst{
+    int data;
+    struct Bst *right, *left;
+    
+}BST;  //BST = Binary Search Tree
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
