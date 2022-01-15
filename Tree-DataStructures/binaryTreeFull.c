6c072b050dff94983792d7d4cd33886a5b326c2e
orhan <orhankagan33@gmail.com>
2022-01-16 00:34:41 +0300

    functions added

--- Tree-DataStructures/binaryTreeFull.c
+++ Tree-DataStructures/binaryTreeFull.c
@@ -19,7 +19,7 @@ int countNodes(BST*,int);
 
 int main()
 {
-    int choice=0,item;
+    int choice=0,item, count = 0;
     BST *tempNode, *tempNode2;
 
     while (1)
@@ -39,7 +39,7 @@ int main()
                 tempNode = readNode();
                 insert(root,tempNode);
                 break;
-            case 2:
+        /*    case 2:
                 printf("enter item to delete: ");
                 scanf("%d",&item);
                 tempNode2 = delete(root,item);
@@ -47,11 +47,11 @@ int main()
                     printf("Item deleted.\n");
                 else
                     printf("Item not found.\n");
-                break;
+                break; */
             case 3:
-                if(root != NULL){
+                if(root != NULL)
                     traverse(root);
-                }else
+                else
                     printf("Tree is empty.\n");
                 break;
             case 4:
@@ -65,7 +65,7 @@ int main()
                     printf("Item is not found.");
                 break;
             case 5:
-                int count = countNodes(root, count);
+                count = countNodes(root, count);
                 printf("Number of nodes: %d",count);
                 break;
             default:
@@ -74,4 +74,76 @@ int main()
 
     }//while
     
+}
+
+
+///*****FUNCTION DEFINITONS******
+BST *readNode(){
+    BST *newNode = (BST*)malloc(sizeof(BST));
+
+    printf("enter value of node: \n");
+    scanf("%d",newNode->data);
+    newNode->right = newNode->left = NULL;
+    return newNode;
+}
+
+int countNodes(BST* treeRoot, int sum){
+    if(treeRoot == NULL){
+        return sum;
+    }
+    sum++;
+    sum = countNodes(treeRoot->left, sum);
+    sum = countNodes(treeRoot->right, sum);
+    return sum;
+}
+
+int getRightMin(BST* treeRoot){
+    BST* temp = treeRoot;
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp->data;
+}
+
+///*****PREORDER TRAVERSAL***
+void traverse(BST* treeRoot){
+    
+    if(treeRoot != NULL){
+        printf("%d\t",treeRoot->data);  //root
+        traverse(treeRoot->left);       //left 
+        traverse(treeRoot->right);      //right
+    }
+}
+
+BST* search(BST* treeRoot, int item){
+    while(treeRoot != NULL && treeRoot->data != item)
+    {
+        if(item < treeRoot->data)
+            treeRoot = treeRoot->left;
+        else
+            treeRoot = treeRoot->right;
+    }
+    return treeRoot;
+}
+
+void insert(BST* treeRoot,BST* newNode){
+    if(treeRoot == NULL){
+        root = newNode;
+    }
+    else{
+        if(newNode->data < treeRoot->data){
+            if(treeRoot->left == NULL)
+                treeRoot->left = newNode;
+            else
+                insert(treeRoot->left, newNode);
+        }
+        else{
+            if(treeRoot->right == NULL)
+                treeRoot->right = newNode;
+            else
+                insert(treeRoot->right, newNode);
+        
+        }
+    }
 }
